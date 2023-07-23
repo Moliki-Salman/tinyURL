83e39ae68cc77e86755bda596f50729c3218386d
Moliki-Salman <salmanameenat@gmail.com>
2023-07-23 19:09:14 +0200

    changes made

--- controller/userController.js
+++ controller/userController.js
@@ -2,7 +2,6 @@ const jwt = require("jsonwebtoken");
 const UserModel = require("../model/userModel");
 const bcrypt = require("bcrypt");
 
-
 const SECRET_KEY = "ABCDEFG";
 
 const signup = async (req, res) => {
@@ -11,12 +10,12 @@ const signup = async (req, res) => {
     const existingUser = await UserModel.findOne({ email: email });
     if (existingUser) {
       return res.status(400).json({ message: "user already exist" });
-    };
+    }
     // const hashedPassword = await bcrypt.hash(password, 10);
-    bcrypt.hash(password, 10, async function(err, hashedPassword) {
+    bcrypt.hash(password, 10, async function (err, hashedPassword) {
       if (err) {
         throw new Error("error", { cause: err });
-      };
+      }
       // Store hash in your password DB.
       const result = await UserModel.create({
         firstname: firstname,
@@ -42,22 +41,25 @@ const signup = async (req, res) => {
 const login = async (req, res) => {
   const { email, password } = req.body;
   try {
-    const existingUser = await UserModel.findOne({ email: email});
+    const existingUser = await UserModel.findOne({ email: email });
     if (!existingUser) {
       return res.status(400).json({ message: "User  not found" });
-    };
+    }
 
     bcrypt.compare(
       password,
       existingUser.password,
       function (err, matchedpassword) {
         if (!matchedpassword) {
-          // const roles = object.values(existingUser.role)
           return res.status(400).json({ mesaage: "Invalid credentials" });
         }
-        
+
         const token = jwt.sign(
-          { email: existingUser.email, id: existingUser._id, role: existingUser.role },
+          {
+            email: existingUser.email,
+            id: existingUser._id,
+            role: existingUser.role,
+          },
           SECRET_KEY
         );
         res.status(201).json({ user: existingUser, token: token });
@@ -65,7 +67,9 @@ const login = async (req, res) => {
     );
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: "something went wrong", error: JSON.stringify(error) });
+    res
+      .status(500)
+      .json({ message: "something went wrong", error: JSON.stringify(error) });
   }
 };
 
--- middleware/Authentication.js
+++ middleware/Authentication.js
@@ -1,17 +1,17 @@
 const jwt = require("jsonwebtoken");
 const SECRET_KEY = "ABCDEFG";
 
- const authenticateToken = async (req, res, next) => {
-  const { authorization } = req.headers
-  const token = authorization.split(" ")[1] 
-  console.log(token)
+const authenticateToken = async (req, res, next) => {
+  const { authorization } = req.headers;
+  const token = authorization.split(" ")[1];
+  console.log(token);
   if (!token) {
-    return res.status(402).json("valid token required")
+    return res.status(402).json("valid token required");
   }
   try {
-    const decodedToken = jwt.verify(token, SECRET_KEY)
-    console.log(decodedToken)
-    req.user = decodedToken
+    const decodedToken = jwt.verify(token, SECRET_KEY);
+    console.log(decodedToken);
+    req.user = decodedToken;
     next();
   } catch (error) {
     res.status(401).json({
@@ -20,19 +20,15 @@ const SECRET_KEY = "ABCDEFG";
   }
 };
 
-
 function checkUserRole(role) {
   return (req, res, next) => {
-     if (req.user.role !== "admin"){
-      res.status(401).json({ message: "this route os restricted to only admin users" })
-     }
-     next()
-  }
- 
-
+    if (req.user.role !== "admin") {
+      res
+        .status(401)
+        .json({ message: "this route os restricted to only admin users" });
+    }
+    next();
+  };
 }
 
-
-module.exports = authenticateToken, checkUserRole
- 
- 
+(module.exports = authenticateToken), checkUserRole;
