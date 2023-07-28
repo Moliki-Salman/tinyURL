9e9ac5ef82f508f5d56600fdb14e633adbf66dee
Moliki-Salman <salmanameenat@gmail.com>
2023-07-28 23:02:24 +0200

    test routes to get a single url

--- test/test-app.js
+++ test/test-app.js
@@ -33,34 +33,50 @@ describe("url", () => {
         longUrl: "https://longurl.com"
       };
       
-    
       chai.request(app)
       .post("/")
       .send(url)
       .end((error, res) => {
-        res.should.have(200);
+        res.should.have.status(200);
         res.body.should.be.a("object")
         res.status.should.be.eql(200);
-        response.should.have.property("urlCode")
-        response.should.have.property("longUrl")
-        response.should.have.property("shortUrl")
-        response.should.have.property("user")
-        response.should.have.property("_id")
+        res.should.have.property("urlCode")
+        res.should.have.property("longUrl")
+        res.should.have.property("shortUrl")
+        res.should.have.property("user")
+        res.should.have.property("_id")
         done();
       });
     });
   });
 
 // test routes  to get a single tiny url
-// describe("GET/:code url", () => {
-//   it("it should GET a single url by the short url code created", async () => {
-//     let url = new url({
-//       shortUrl: "https://shorturl.com"
-//     });
-//     await url.save()
+describe("GET/:code url", () => {
+  it("it should GET a single url by the short url code created by redirect", async () => {
+    let url = {
+      shortUrl: "https://shorturl.com"
+    };
+    
+    chai.request(app)
+    .get("/" + url.urlcode)
+    .send(url)
+    .end((error, res) => {
+      res.should.have.status(200);
+      res.should.have.redirect(308)
+      res.status.should.be.eql(200);
+      done();
+    })
+  })
+})
 
-//   })
-// })
+it("it should not GET a single url by the short url code created by redirect", () => {
+  chai.request(app)
+  .get("/")
+  .end((error, res) => {
+    res.should.have.status(308);
+    done()
+  })
+})
   
 
 
