034c0d39c97ffef7373aa2994f276af281837535
Moliki-Salman <salmanameenat@gmail.com>
2023-07-28 17:19:59 +0200

    test on get all user's urls

---
+++ test/test-app.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+let urlModel = require("../model/urlModel");
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const app = require("../app");
+chai.should();
+
+chai.use(chaiHttp);
+// empty the database before test is carried out
+describe("url", () => {
+  beforeEach(async () => {
+    await urlModel.deleteMany({});
+  });
+  //get url routes
+  describe("/GET url", () => {
+    it("it should GET all the urls that belongs to a user", () => {
+      chai
+        .request(app)
+        .get("/")
+        .end((error, res) => {
+          res.should.have.status(200);
+          res.body.data.should.be.a("array");
+          res.body.data.length.should.be.eql(0);
+          done();
+        });
+    });
+  });
+});
