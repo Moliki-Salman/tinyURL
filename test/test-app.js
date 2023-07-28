2d35230effda893204f60e53cd9c6e21e3c53d11
Moliki-Salman <salmanameenat@gmail.com>
2023-07-28 21:07:35 +0200

    updated test create tiny url

--- test/test-app.js
+++ test/test-app.js
@@ -11,8 +11,8 @@ describe("url", () => {
   beforeEach(async () => {
     await urlModel.deleteMany({});
   });
-  // test routes  to get all urls 
-  describe("/GET url", () => {
+  // test routes  to get all tiny urls 
+  describe("GET url", () => {
     it("it should GET all the urls that belongs to a user", () => {
       chai
         .request(app)
@@ -26,11 +26,14 @@ describe("url", () => {
     });
   });
 
+// test routes  to create a tiny url
   describe("POST url", () => {
-    it("It should create a new shortUrl", () => {
+    it("It should create a new shortUrl",  () => {
       let url = {
         longUrl: "https://longurl.com"
       };
+      
+    
       chai.request(app)
       .post("/")
       .send(url)
@@ -38,10 +41,27 @@ describe("url", () => {
         res.should.have(200);
         res.body.should.be.a("object")
         res.status.should.be.eql(200);
+        response.should.have.property("urlCode")
+        response.should.have.property("longUrl")
+        response.should.have.property("shortUrl")
+        response.should.have.property("user")
+        response.should.have.property("_id")
         done();
       });
     });
   });
+
+// test routes  to get a single tiny url
+// describe("GET/:code url", () => {
+//   it("it should GET a single url by the short url code created", async () => {
+//     let url = new url({
+//       shortUrl: "https://shorturl.com"
+//     });
+//     await url.save()
+
+//   })
+// })
+  
 
 
 
