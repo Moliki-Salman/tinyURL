6513ee5c55e21c3feeafa23f76781d96284cc0f1
Moliki-Salman <salmanameenat@gmail.com>
2023-08-22 11:27:58 +0200

    authentication test

--- test/auth.test.js
+++ test/auth.test.js
@@ -4,24 +4,27 @@ const userModel = require("../models/user-model");
 const jwt = require("jsonwebtoken");
 const chai = require("chai");
 const expect = chai.expect;
-const chaiHttp = require("chai-http");
-const app = require("../app");
-chai.use(chaiHttp);
 
 describe("authenticate a user", async function () {
   it("should set authentication to equal a valid token", async function () {
-    let req = {
-      user: {
-        email: "morayo@gmail.com",
-        password: "1234",
-      },
-    };
+    let req = { user: { email: "morayo@gmail.com", password: "1234" } };
     await userModel.findOne({ email: req.user.email });
     let token = jwt.sign({ email: req.user.email }, process.env.SECRET_KEY);
     let authorization = req.headers;
     authorization = `Bearer ${token}`;
 
-    expect(token).to.be.a("string");
+    expect(authorization).to.be.a("string");
     expect(authorization).to.equal(`Bearer ${token}`);
+  });
+
+  it("should verify a user with the correct docoded token", async function () {
+    let req = { user: { email: "morayo@gmail.com", password: "1234" } };
+    await userModel.findOne({ email: req.user.email });
+    let token = jwt.sign({ email: req.user.email }, process.env.SECRET_KEY);
+    let decodedToken = jwt.verify(token, process.env.SECRET_KEY);
+    decodedToken = req.user;
+
+    expect(decodedToken).to.be.a("object");
+    expect(req.user).to.be.equal(decodedToken);
   });
 });
