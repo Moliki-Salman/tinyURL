36da6eb0729d66dcc835af4397e579aefd02b326
Moliki-Salman <salmanameenat@gmail.com>
2023-08-28 12:36:20 +0200

    test to check the invalidity of a long url

--- test/url.test.js
+++ test/url.test.js
@@ -10,8 +10,8 @@ const chaiHttp = require("chai-http");
 chai.use(chaiHttp);
 
 describe("url collections", async function () {
-  it("should create a short URL with valid authentication", async function () {
-    const user = { email: "yunus@gmail.com", password: "1234" };
+  it("should create a tiny URL with valid authentication", async function () {
+    const user = { email: "newUser1@gmail.com", password: "1234" };
     const token = jwt.sign(user, process.env.SECRET_KEY);
     const longUrl =
       "https://www.youtube.com/playlist?list=PL3IwAics3J0dc9Yav3MMqUlq6RmdC5-I_";
@@ -26,20 +26,32 @@ describe("url collections", async function () {
         expect(res.body).to.have.property("shortUrl");
       });
   });
-  
- it("should return an error if user authentication is invalid", async () => {
-  const longUrl = "https://www.youtube.com/playlist?list=PL3IwAics3J0dc9Yav3MMqUlq6RmdC5-I_";
+
+  it("should return an error if user authentication is invalid", async () => {
+    const longUrl =
+      "https://www.youtube.com/playlist?list=PL3IwAics3J0dc9Yav3MMqUlq6RmdC5-I_";
 
     chai
       .request(app)
       .post("/")
       .send({ longUrl })
       .end((err, res) => {
-        expect(res).to.have.status(401);
-        expect(res.body).to.have.property("message", "Authentication failed");;
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.property("message", "Authentication failed");
       });
   });
 
+  it("should return an error is a long url is invalid", async function () {
+    const user = { email: "newUser2", password: "1234" };
+    const token = jwt.sign(user, process.env.SECRET_KEY);
+    const longUrl = "invalidurl";
+     const response = await chai
+      .request(app)
+      .post('/')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ longUrl });
 
-
+    expect(response).to.have.status(401);
+    expect(response.body).to.equal('long url is not valid');
+  });
 });
