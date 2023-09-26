54213c0041b1fd5e300547ee0295314c39ce824e
ydaniju <yusuf.daniju@doctolib.com>
2023-09-26 18:41:34 +0200

    remove controller test

--- __tests__/models/url-model.test.js
+++ __tests__/models/url-model.test.js
@@ -2,7 +2,6 @@ const Url = require('../../models/url-model')
 
 describe('url', () => {
   describe('validations', () => {
-    // @Moliki-Salman: The url model allows a url to exist without url or user
     it('is valid without any input from url', async () => {
       const url = new Url()
       const errors = await url.validate()
--- __tests__/models/user-model.test.js
+++ __tests__/models/user-model.test.js
@@ -2,7 +2,6 @@ const User = require('../../models/user-model')
 
 describe('User', () => {
   describe('validations', () => {
-    // @Moliki-Salman: The user model allows someone to exist without name or email
     it('is valid without any input from user', async () => {
       const user = new User()
       const errors = await user.validate()
