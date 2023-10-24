790748dff5f21bf5c852120655a50e96b5663f87
ydaniju <yusuf.daniju@doctolib.com>
2023-10-24 18:48:40 +0200

    format code

--- __tests__/controllers/user-controller.test.js
+++ __tests__/controllers/user-controller.test.js
@@ -1,33 +1,33 @@
 const UserController = require("../../controllers/user-controller");
-const UserModel = require("../../models/user-model")
+const UserModel = require("../../models/user-model");
 
 jest.mock("../../models/user-model");
 
 describe("signup", () => {
   it("should return a 400 status response if a user already exist", async () => {
     UserModel.findOne = jest.fn().mockResolvedValue({});
-    
+
     const req = {
       body: {
-       firstname: "Sarah",
-       lastname: "Morgan",
-       email: "sarah.m@gmail.com",
-       password: "password0923"
+        firstname: "Sarah",
+        lastname: "Morgan",
+        email: "sarah.m@gmail.com",
+        password: "password0923",
       },
     };
- 
 
     const res = {
       status: jest.fn().mockReturnThis(),
       json: jest.fn(),
-    }
+    };
 
-// the error says userController is not a function
-    await UserController(req, res); 
+    // the error says userController is not a function
+    await UserController(req, res);
 
-    expect(UserModel.findOne).toHaveBeenCalledWith({ email: "sarah.m@gmail.com" });
+    expect(UserModel.findOne).toHaveBeenCalledWith({
+      email: "sarah.m@gmail.com",
+    });
     expect(res.status).toHaveBeenCalledWith(400);
-    expect(res.json).toHaveBeenCalledWith("user already exists")
+    expect(res.json).toHaveBeenCalledWith("user already exists");
   });
-
-})
+});
