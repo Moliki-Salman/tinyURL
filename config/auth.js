b7f9c1f1f955aef6a2057b29ebae43eb5451c475
Moliki-Salman <salmanameenat@gmail.com>
2025-06-02 13:08:32 +0200

    added more query to auth

--- config/auth.js
+++ config/auth.js
@@ -5,9 +5,16 @@ config();
 
 export const authenticateUser = async (req, res, next) => {
   const { authorization } = req.headers;
+  if (!authorization) {
+    return res
+      .status(401)
+      .json({ message: "No authorization token provided." });
+  }
   const token = authorization.split(" ")[1];
   if (!token) {
-    return res.status(404).json({ message: "valid token required" });
+    return res.status(404).json({
+      message: "valid token required (Bearer token format expected).",
+    });
   }
   try {
     const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
@@ -18,4 +25,3 @@ export const authenticateUser = async (req, res, next) => {
     return res.status(401).json({ message: "Authentication failed" });
   }
 };
-
